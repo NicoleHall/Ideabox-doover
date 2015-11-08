6daca085307d84190e62d1d8e08a2c83f84f12d3
Nicole Hall <nicolealexandrahall@gmail.com>
2015-11-08 13:08:56 -0700

    added sad path for non existant user.  Test pass

--- ideabox/test/integration/user_login_test.rb
+++ ideabox/test/integration/user_login_test.rb
@@ -2,15 +2,21 @@ require 'test_helper'
 
 class UserLoginTest < ActionDispatch::IntegrationTest
   test 'registered user can login in' do
-    user = User.create(username: "MarloMajor", password: "password")
-
-    visit login_path
-    fill_in "Username", with: user.username
-    fill_in "Password", with: "password"
-    click_button "Login"
+    user_is_created_and_logged_in
 
     assert page.has_content?("Hello MarloMajor")
 
+  end
+
+  test 'user does not exist shows error' do
+    visit login_path
+    fill_in "Username", with: "Nobody"
+    fill_in "Password", with: "password"
+    assert page.has_content?("Login")
+    click_button "Login"
+    assert page.has_content?("Login")
+    save_and_open_page
+    assert page.has_content?("Incorrect Username or Password")
   end
 
   # test for if user does not exist
