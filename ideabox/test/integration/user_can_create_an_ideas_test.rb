11552b1656f714fea0e73ebf3c42070780e63b97
Nicole Hall <nicolealexandrahall@gmail.com>
2015-11-08 18:31:08 -0700

    relationship properly set up to confirm that user can respond to idea query.  Test Passes.

--- ideabox/test/integration/user_can_create_an_ideas_test.rb
+++ ideabox/test/integration/user_can_create_an_ideas_test.rb
@@ -11,10 +11,18 @@ class UserCanCreateIdeasTest < ActionDispatch::IntegrationTest
     click_button "Create Idea"
 
     idea = Idea.create!(idea_name: "Electric Car", idea_description: "Electric Car")
-save_and_open_page
+
     assert page.has_content? "Good Idea!"
     assert page.has_content? "Electric Car"
     assert page.has_content? "Apple Car"
     assert current_path, idea_path(:id)
+  end
+
+  test 'user can respond to idea query' do
+    user = User.create(username: "MarloMajor", password: "password")
+    idea = Idea.create(idea_name: "Electric Car", idea_description: "Apple Car", user: user)
+
+    assert idea.respond_to?(:user_id)
+
   end
 end
